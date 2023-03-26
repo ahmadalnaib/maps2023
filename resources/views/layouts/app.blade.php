f714425847424b5b48807c3e823c2a58419f8464
ahmedhabeeb73 <alnaib888@gmail.com>
2023-03-26 12:44:01 +0200

    add alert

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,7 @@
 
         <!-- Styles -->
         @livewireStyles
+        <script src="https://kit.fontawesome.com/ca11177b7a.js" crossorigin="anonymous"></script>
         <script src="https://code.jquery.com/jquery-3.6.4.min.js" integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script>
     </head>
     <body class="font-sans antialiased">
