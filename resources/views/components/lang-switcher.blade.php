7427e58b2464f321bb848ab494433a9f49e86938
ahmad.al-naib <ahmad.al-naib@sitec.de>
2023-04-19 12:08:28 +0200

    edit the navbar

--- resources/views/components/lang-switcher.blade.php
+++ resources/views/components/lang-switcher.blade.php
@@ -1,4 +1,4 @@
-<nav class="relative" x-data="{ open: false }" @click.away="open = false" style="margin-left: -20px; margin-top:-2px">
+{{-- <nav class="relative" x-data="{ open: false }" @click.away="open = false" style="margin-left: -20px; margin-top:-2px">
     <button type="button" class="inline-flex items-center justify-center w-full px-1 py-5 text-sm font-medium text-white rounded-md focus:outline-none " @click="open = !open">
       <svg class="w-6 h-6 mr-1" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
         <path d="M49.9583 8.33334C26.9583 8.33334 8.33334 27 8.33334 50C8.33334 73 26.9583 91.6667 49.9583 91.6667C73 91.6667 91.6667 73 91.6667 50C91.6667 27 73 8.33334 49.9583 8.33334ZM78.8333 33.3333H66.5417C65.2376 28.1727 63.3066 23.1912 60.7917 18.5C68.3921 21.1167 74.7965 26.3822 78.8333 33.3333ZM50 16.8333C53.4583 21.8333 56.1667 27.375 57.9583 33.3333H42.0417C43.8333 27.375 46.5417 21.8333 50 16.8333ZM17.75 58.3333C17.0833 55.6667 16.6667 52.875 16.6667 50C16.6667 47.125 17.0833 44.3333 17.75 41.6667H31.8333C31.5 44.4167 31.25 47.1667 31.25 50C31.25 52.8333 31.5 55.5833 31.8333 58.3333H17.75ZM21.1667 66.6667H33.4583C34.7917 71.875 36.7083 76.875 39.2083 81.5C31.5999 78.8976 25.191 73.6284 21.1667 66.6667ZM33.4583 33.3333H21.1667C25.191 26.3716 31.5999 21.1024 39.2083 18.5C36.6934 23.1912 34.7624 28.1727 33.4583 33.3333ZM50 83.1667C46.5417 78.1667 43.8333 72.625 42.0417 66.6667H57.9583C56.1667 72.625 53.4583 78.1667 50 83.1667ZM59.75 58.3333H40.25C39.875 55.5833 39.5833 52.8333 39.5833 50C39.5833 47.1667 39.875 44.375 40.25 41.6667H59.75C60.125 44.375 60.4167 47.1667 60.4167 50C60.4167 52.8333 60.125 55.5833 59.75 58.3333ZM60.7917 81.5C63.2917 76.875 65.2083 71.875 66.5417 66.6667H78.8333C74.7965 73.6178 68.3921 78.8833 60.7917 81.5ZM68.1667 58.3333C68.5 55.5833 68.75 52.8333 68.75 50C68.75 47.1667 68.5 44.4167 68.1667 41.6667H82.25C82.9167 44.3333 83.3333 47.125 83.3333 50C83.3333 52.875 82.9167 55.6667 82.25 58.3333H68.1667Z" fill="white"/>
@@ -18,5 +18,72 @@
         @endif
       @endforeach
     </nav>
-  </nav>
+  </nav> --}}
   
+<div class="flex justify-center" style="margin-left: -20px; margin-top:9px">
+  <div
+      x-data="{
+          open: false,
+          toggle() {
+              if (this.open) {
+                  return this.close()
+              }
+
+              this.$refs.button.focus()
+
+              this.open = true
+          },
+          close(focusAfter) {
+              if (! this.open) return
+
+              this.open = false
+
+              focusAfter && focusAfter.focus()
+          }
+      }"
+      x-on:keydown.escape.prevent.stop="close($refs.button)"
+      x-on:focusin.window="! $refs.panel.contains($event.target) && close()"
+      x-id="['dropdown-button']"
+      class="relative"
+  >
+      <!-- Button -->
+      <button
+          x-ref="button"
+          x-on:click="toggle()"
+          :aria-expanded="open"
+          :aria-controls="$id('dropdown-button')"
+          type="button"
+          class="flex items-center gap-2 px-5 py-2.5 rounded-md "
+      >
+
+      {{ config('locales.languages')[app()->getLocale()]['name'] }}
+
+          <!-- Heroicon: chevron-down -->
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="
+          #fff">
+              <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+          </svg>
+      </button>
+
+      <!-- Panel -->
+      <div
+          x-ref="panel"
+          x-show="open"
+          x-transition.origin.top.left
+          x-on:click.outside="close($refs.button)"
+          :id="$id('dropdown-button')"
+          style="display: none;"
+          class="absolute left-0 mt-2 w-40 rounded-md bg-white shadow-md"
+      >
+      @foreach (config('locales.languages') as $key => $lang)
+      @if ($key !== app()->getLocale())
+      
+        <a href="{{ route('change.language', $key) }}" class="flex items-center gap-2 w-full first-of-type:rounded-t-md last-of-type:rounded-b-md px-4 py-2.5 text-left text-sm hover:bg-gray-50 disabled:text-gray-500">
+          <span class="text-red-600">{{ $lang['name'] }}</span>
+      </a>
+      @endif
+    @endforeach
+        
+      </div>
+  </div>
+</div>
