bc4a8577be3f3d69b490f27daf68f0866ada32ca
ahmad.al-naib <ahmad.al-naib@sitec.de>
2023-05-16 11:03:56 +0200

    change the imperstions

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -21,6 +21,27 @@
     </head>
     <body >
         <x-banner />
+        @if(session()->has('impersonate'))
+        <div class="relative bg-indigo-600">
+            <div class="max-w-screen-xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
+                <div class="pr-16 sm:text-center sm:px-16">
+                    <p class="font-medium text-white">
+                        <span class="md:hidden">
+                            You are impersonating {{auth()->user()->name}}
+                        </span>
+                        <span class="hidden md:inline">
+                            You are impersonating {{auth()->user()->name}}
+                        </span>
+                        <span class="block sm:ml-2 sm:inline-block">
+                            <a href="{{route('leave-impersonation')}}" class="text-white font-bold underline">
+                                Leave Impersonation &rarr;
+                            </a>
+                        </span>
+                    </p>
+                </div>
+            </div>
+        </div>
+        @endif
 
         <div class="min-h-screen bg-white">
             @include('includes.navigation-menu-admin')
