b873b0a680a88eb94b0769430b1af51f38202204
ahmad.al-naib <ahmad.al-naib@sitec.de>
2023-05-22 17:08:13 +0200

    last check

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -50,7 +50,7 @@
             <label for="rental_period">Select rental period:</label>
             <select name="rental_period" id="rental_period" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                 @foreach ($plans as $plan)
-                    <option value="{{ $plan->id }}">{{ $plan->name }} - {{$plan->price}} </option>
+                    <option value="{{ $plan->id }}">{{ $plan->name }} - {{$plan->price}} &#x20AC</option>
                 @endforeach
             </select>
         </div>
