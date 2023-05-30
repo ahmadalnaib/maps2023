29179a9b21b35700b7882d4abef43b8e3e3555d1
ahmad.al-naib <ahmad.al-naib@sitec.de>
2023-05-30 17:02:30 +0200

    pages

---
+++ app/Models/Price.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use App\Scopes\TenantScope;
+use App\Traits\BelongsToTenant;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Price extends Model
+{
+    use HasFactory,BelongsToTenant ;
+   
+}
