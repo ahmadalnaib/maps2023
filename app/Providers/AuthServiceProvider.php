be88916479032225c6c39ce7844a469b54684637
ahmad.al-naib <ahmad.al-naib@sitec.de>
2023-06-12 16:00:15 +0200

    add more tran

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -6,7 +6,9 @@ use App\Models\Team;
 use App\Policies\TeamPolicy;
 use App\Policies\UserPolicy;
 use Laravel\Passport\Passport;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
@@ -35,7 +37,17 @@ class AuthServiceProvider extends ServiceProvider
                 ->line('Klicken Sie auf die Schaltfläche unten, um Ihre E-Mail-Adresse zu bestätigen.')
                 ->action('E-Mail Adresse überprüfen', $url);
         });
-    
+
+        ResetPassword::toMailUsing(function ($notifiable, $url) {
+            $resetUrl = URL::signedRoute('password.reset', ['token' => $url]);
+        
+            return (new MailMessage)
+                ->subject('Passwort zurücksetzen angefordert')
+                ->line('Sie erhalten diese E-Mail, weil wir eine Anfrage zum Zurücksetzen des Passworts für Ihr Konto erhalten haben.')
+                ->action('Passwort zurücksetzen', $resetUrl)
+                ->line('Dieser Link zum Zurücksetzen des Passworts wird in 60 Minuten ablaufen.')
+                ->line('Wenn Sie keine Rücksetzung des Passworts beantragt haben, sind keine weiteren Schritte erforderlich.');
+        });
 
         //
     }
