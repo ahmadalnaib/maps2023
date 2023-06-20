4fa5785e62dbbe2d57f677243d66a359a0bb03e2
ahmad.al-naib <ahmad.al-naib@sitec.de>
2023-06-20 10:13:45 +0200

    fixed the trans

--- resources/lang/de/index.php
+++ resources/lang/de/index.php
@@ -2,8 +2,8 @@
 
 return[
   'find' => 'Finde ein Fach in deiner Nähe',
-  'find suitable lockers with address or postal code'=>'Mit Adresse order PLZ zeigen wir dir passende Fächer.',
-  'Lockers for left-luggage and bikes are available on different locations'=>'Passende Fächer für Gepäck, Taschen und Fahrradräder findest du an unterschiedlichen Standorten.',
+  'find suitable lockers with address or postal code'=>'Mit Adresse oder PLZ zeigen wir dir passende Fächer.',
+  'Lockers for left-luggage and bikes are available on different locations'=>'Passende Fächer für Gepäck, Taschen und Fahrräder findest du an unterschiedlichen Standorten.',
   'Enter Address or Postcode'=>'Adresse oder Postleitzahl eingeben',
   'Select State/City'=>'Stadt auswählen',
   'Search'=>"Suche",
